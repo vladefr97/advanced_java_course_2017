0240cbe7db9eeb1cd964fb86d4193513001ad02f
Dmitry <dmitry.drobotov123@gmail.com>
2017-10-13 00:46:43 +0300

    Implement CustomBigInteger

--- season1/episode0/homework/src/main/java/edu/technopolis/FibonacciSolver.java
+++ season1/episode0/homework/src/main/java/edu/technopolis/FibonacciSolver.java
@@ -1,4 +1,22 @@
 package edu.technopolis;
 
-public class FibonacciSolver {
+public class FibonacciSolver implements FibonacciAlgorithm {
+
+    @Override
+    public String evaluate(int index) {
+        if (index < 3) {
+            return "1";
+        }
+        CustomBigInteger one = new CustomBigInteger(1L);
+        CustomBigInteger two = new CustomBigInteger(1L);
+        CustomBigInteger result = one;
+        for (int i = 2; i < index; i++) {
+            result = one.add(two);
+            one = two;
+            two = result;
+        }
+
+        return result.toString();
+    }
+
 }
