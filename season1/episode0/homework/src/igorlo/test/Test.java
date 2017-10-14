9f7294ab6f0c939f4ad97098d47abe3e42a94ce0
igorlo <i440533>
2017-10-14 20:13:50 +0300

    Сделал домашнее задание

--- season1/episode0/homework/src/igorlo/test/Test.java
+++ season1/episode0/homework/src/igorlo/test/Test.java
@@ -9,7 +9,7 @@ public class Test {
     public static void main(String[] args) {
 
         boolean shouldPrintProcess = true;
-        testAll(true);
+        testAll(shouldPrintProcess);
 
 
     }
